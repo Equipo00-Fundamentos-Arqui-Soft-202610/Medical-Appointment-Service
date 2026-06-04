7074acc62f9f2d51eef646b6edf45cfcf726e1d9
jquijada-dev <jeremyquijama20@gmail.com>
2026-06-03 21:09:41 -0500

    fix: configuraciones-mysql

--- MediTrack.MedicalAppointmentService.API/obj/Debug/net8.0/MediTrack.MedicalAppointmentService.API.AssemblyInfo.cs
+++ MediTrack.MedicalAppointmentService.API/obj/Debug/net8.0/MediTrack.MedicalAppointmentService.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MediTrack.MedicalAppointmentService.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df2ec57654737146656adce20101990c26917f34")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a454ec5b23f78035b1d541dabea59b5f2ff378d3")]
 [assembly: System.Reflection.AssemblyProductAttribute("MediTrack.MedicalAppointmentService.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MediTrack.MedicalAppointmentService.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
